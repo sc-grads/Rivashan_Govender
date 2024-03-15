1bc466117e4aeefbf841626e947bc157a7b4c9dc
Rivi09 <160615283+Rivi09@users.noreply.github.com>
2024-03-15 04:16:45 +0200

    Update Add_Table.sql

--- scripts/Add_Table.sql
+++ scripts/Add_Table.sql
@@ -1,16 +1,27 @@
 USE AutomatedDatabase
 go
 
-BEGIN
 
-IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'UserInfo')
-CREATE TABLE UserInfo
-(	
-	UserID INT NOT NULL,
-	Name VARCHAR(50) NOT NULL
-)
-END 
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Employee_Details')
+CREATE TABLE Employee_Details
 
-BEGIN
-INSERT INTO UserInfo (UserId, Name)VALUES (1, 'Elon');
-END
+-- Create UserInfo table with additional columns
+CREATE TABLE Employee_Details
+(
+    EmpID INT NOT NULL,
+    Name VARCHAR(50) NOT NULL,
+    Surname VARCHAR(50),
+    [Date Of Empoyment] DATE,
+    Position VARCHAR(50) CHECK (Position IN ('Representative', 'Agent', 'Manager'))
+);
+
+
+-- Insert data into UserInfo table with new columns
+INSERT INTO Employee_Details (EmpID, Name, Surname, [Date Of Empoyment], Position)
+VALUES 
+(1234, 'Elon', 'Musk', '2001-06-28', 'Manager'),
+(2341, 'Ted', 'Mosby', '2006-04-27', 'Agent'),
+(3412, 'Chandler', 'Bing', '2023-04-16', 'Agent'),
+(4512, 'Akira', 'Toriyama', '2002-01-01', 'Manager'),
+(5123, 'Peppa', 'Pig', '2024-03-15', 'Representative');
+
