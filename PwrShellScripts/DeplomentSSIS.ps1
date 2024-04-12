03a61207c20804d1eef72cfcd317880b36d97521
Rivi09 <160615283+Rivi09@users.noreply.github.com>
2024-04-12 12:23:04 +0200

    Update DeplomentSSIS.ps1

--- PwrShellScripts/DeplomentSSIS.ps1
+++ PwrShellScripts/DeplomentSSIS.ps1
@@ -1,21 +1,24 @@
 # Variables
 $SSISNamespace = "Microsoft.SqlServer.Management.IntegrationServices"
-$TargetServerName = "DESKTOP-932FK41"
+$TargetServerName = "0.tcp.eu.ngrok.io,10013"  # Update with your server name and port
+$TargetDatabase = "GraduateDB"  # Update with your target database name
 $TargetFolderName = "GraduateDB"
-$ProjectFilePath = "C:\Users\Rivashan Govender\Documents\Rivashan_Govender\DatabaseAdministration\SSIS\GraduateMultiFile\GraduateMultiFile\bin\Development\GraduateMultiFile.ispac"
+$ProjectFilePath = "DatabaseAdministration/SSIS/ispac/GraduateMultiFile.ispac"
 $ProjectName = "GraduateMultiFile"
+$Username = "AutomationUser"
+$Password = "Bow34908"
+
 
 
 Add-Type -Path "C:\Program Files (x86)\Microsoft SQL Server Management Studio 19\Common7\IDE\CommonExtensions\Microsoft\SSIS\160\Binn\150References\SMO\Microsoft.SqlServer.Dmf.Common.dll"
 
+# Create the SQL connection string with SQL Server Authentication
+$sqlConnectionString = "Data Source=$TargetServerName;Initial Catalog=$TargetDatabase;User ID=$Username;Password=$Password;"
+$sqlConnection = New-Object System.Data.SqlClient.SqlConnection $sqlConnectionString
+
 # Load the IntegrationServices assembly
 $loadStatus = [System.Reflection.Assembly]::Load("Microsoft.SQLServer.Management.IntegrationServices, "+
     "Version=16.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91, processorArchitecture=MSIL")
-
-# Create a connection to the server
-$sqlConnectionString = "Data Source=" + $TargetServerName + ";Initial Catalog=master;Integrated Security=SSPI;"
-$sqlConnection = New-Object System.Data.SqlClient.SqlConnection $sqlConnectionString
-
 
 # Create the Integration Services object
 $integrationServices = New-Object $SSISNamespace".IntegrationServices" $sqlConnection
@@ -24,14 +27,14 @@ $integrationServices = New-Object $SSISNamespace".IntegrationServices" $sqlConne
 $catalog = $integrationServices.Catalogs["SSISDB"]
 
 # Create the target folder
-$folder = New-Object $SSISNamespace".CatalogFolder" ($catalog, $TargetFolderName,
-    "Folder description")
+$folder = New-Object $SSISNamespace".CatalogFolder" ($catalog, $TargetFolderName, "Folder description")
 $folder.Create()
 
-Write-Host "Deploying " $ProjectName " project ..."
+Write-Host "Deploying $ProjectName project ..."
 
 # Read the project file and deploy it
 [byte[]] $projectFile = [System.IO.File]::ReadAllBytes($ProjectFilePath)
 $folder.DeployProject($ProjectName, $projectFile)
 
+Write-Host "Done."
 Write-Host "Done."
