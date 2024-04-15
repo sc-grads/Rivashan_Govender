f9b46f2dbf3736d5669d57f0f3b031a6d1ff71a6
Rivi09 <160615283+Rivi09@users.noreply.github.com>
2024-04-15 11:52:54 +0200

    Update SSISPackageDeployer.java

--- scripts/SSISPackageDeployer.java
+++ scripts/SSISPackageDeployer.java
@@ -12,6 +12,8 @@ public class SSISPackageDeployer {
         String password = "Bow34908";
 
         String packageFilePath = "DatabaseAdministration/SSIS/ispac/GraduateMultiFile.ispac";
+        Path path = Paths.get(packageFilePath);
+        byte[] packageFileBytes = Files.readAllBytes(path);
 
         try {
             // Connect to SSMS
@@ -24,7 +26,7 @@ public class SSISPackageDeployer {
             String sql = "DECLARE @folder_id uniqueidentifier = NULL;" +
                          "EXEC [catalog].[deploy_project] @folder_name=N'" + GraduateDB + "', @project_name=N'" + GraduateMultiFile+ "', " +
                          "@folder_id=@folder_id, @project_stream=0x" +
-                         "GraduateMultiFile.ispac", // Replace with the binary stream of your .ispac file
+                         new String(packageFileBytes) +  // Replace with the binary stream of your .ispac file
                          "@operation_id=@operation_id OUTPUT, @operation_messages=@operation_messages OUTPUT";
             stmt.executeUpdate(sql);
 
