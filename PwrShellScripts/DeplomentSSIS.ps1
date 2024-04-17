06f542b33c8ca980ff10de17968fa4e73e84aff3
rivashan <rivashan.govender@gmail.com>
2024-04-17 17:01:19 +0200

    Mastering SSIS packages

--- PwrShellScripts/DeplomentSSIS.ps1
+++ PwrShellScripts/DeplomentSSIS.ps1
@@ -1,14 +1,20 @@
 # Variables
-$SSISNamespace = "DatabaseAdministration/SSIS/Microsoft.SqlServer.Management.IntegrationServices.dll"
+$SSISNamespace = "Microsoft.SqlServer.Management.IntegrationServices"
 $TargetServerName = "0.tcp.eu.ngrok.io,10013"  # Update with your server name and port
 $TargetDatabase = "GraduateDB"  # Update with your target database name
 $TargetFolderName = "GraduateDB"
+$ProjectFilePath = "C:\Users\Rivashan Govender\Documents\SSIS\GraduateMultiFile\GraduateMultiFile\bin\Development\GraduateMultiFile.ispac"
+=======
 $ProjectFilePath = "DatabaseAdministration/SSIS/ispac/GraduateMultiFile.ispac"
+>>>>>>> d979507207b9ae8b8c4f67965cc2b0ad68a1f18b
 $ProjectName = "GraduateMultiFile"
 $Username = "AutomationUser"
 $Password = "Bow34908"
 
+
+
 Add-Type -Path "C:\Program Files (x86)\Microsoft SQL Server Management Studio 19\Common7\IDE\CommonExtensions\Microsoft\SSIS\160\Binn\150References\SMO\Microsoft.SqlServer.Dmf.Common.dll"
+
 # Create the SQL connection string with SQL Server Authentication
 $sqlConnectionString = "Data Source=$TargetServerName;Initial Catalog=$TargetDatabase;User ID=$Username;Password=$Password;"
 $sqlConnection = New-Object System.Data.SqlClient.SqlConnection $sqlConnectionString
@@ -17,11 +23,15 @@ $sqlConnection = New-Object System.Data.SqlClient.SqlConnection $sqlConnectionSt
 $loadStatus = [System.Reflection.Assembly]::Load("Microsoft.SQLServer.Management.IntegrationServices, "+
     "Version=16.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91, processorArchitecture=MSIL")
 
+<<<<<<< HEAD
 # Create a connection to the server
 $sqlConnectionString = "Data Source=" + $TargetServerName + ";Initial Catalog=master;Integrated Security=SSPI;"
 $sqlConnection = New-Object System.Data.SqlClient.SqlConnection $sqlConnectionString
 
 
+
+=======
+>>>>>>> d979507207b9ae8b8c4f67965cc2b0ad68a1f18b
 # Create the Integration Services object
 $integrationServices = New-Object $SSISNamespace".IntegrationServices" $sqlConnection
 
@@ -39,5 +49,9 @@ Write-Host "Deploying $ProjectName project ..."
 $folder.DeployProject($ProjectName, $projectFile)
 
 Write-Host "Done."
+<<<<<<< HEAD
 
 
+=======
+Write-Host "Done."
+>>>>>>> d979507207b9ae8b8c4f67965cc2b0ad68a1f18b
